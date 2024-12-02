68ffc7d474e2b92557eff1972f786a40006cdfcd
Daniel Moraes <daniel.c.mors@gmail.com>
2024-12-02 17:13:29 -0300

    Subiu

--- src/app/screens/home/HomeScreen.tsx
+++ src/app/screens/home/HomeScreen.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export function HomeScreen() {
     return (
         <div className="container max-w-screen-xl mx-auto px-4 py-12">
@@ -44,6 +42,38 @@ export function HomeScreen() {
                         <span className="sr-only">Next</span>
                     </span>
                 </button>
+            </div>
+
+
+
+
+            <div className="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 pt-1">
+
+                <div className="border-b border-gray-200">
+                    <nav className="flex gap-x-1" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
+                        <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary py-2 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-primary focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none active" id="tabs-with-underline-item-1" aria-selected="true" data-hs-tab="#tabs-with-underline-1" aria-controls="tabs-with-underline-1" role="tab">
+                            Detalhes
+                        </button>
+                        <button type="button" className="hs-tab-active:font-semibold hs-tab-active:border-primary hs-tab-active:text-primary py-2 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-primary focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none" id="tabs-with-underline-item-2" aria-selected="false" data-hs-tab="#tabs-with-underline-2" aria-controls="tabs-with-underline-2" role="tab">
+                            Tenho interesse
+                        </button>
+                    </nav>
+                </div>
+
+                <div className="mt-3">
+                    <div id="tabs-with-underline-1" role="tabpanel" aria-labelledby="tabs-with-underline-item-1">
+                        <p className="text-gray-500">
+                            Produto: Bicicletinha do vô
+                        </p>
+                        <p>Descrição: Procuro bicicleta do vô em velho estado</p>
+                    </div>
+                    <div id="tabs-with-underline-2" className="hidden" role="tabpanel" aria-labelledby="tabs-with-underline-item-2">
+                        <p className="text-gray-500">
+                            This is the <em className="font-semibold text-gray-800">second</em> item's tab body.
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
