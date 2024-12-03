5feb503245e530d8c0a41bf34947e36fbded39d8
Daniel Moraes <daniel.c.mors@gmail.com>
2024-12-03 10:21:55 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./screens/components/navbar";
+import { SideBar } from "./screens/components/sidebar";
 import { HomeScreen } from "./screens/home/HomeScreen";
 
 export default function Home() {
