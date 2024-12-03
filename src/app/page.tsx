7886f41ec4b709b7b6f326971e5dd9f62ef2ff8d
Daniel Moraes <daniel.c.mors@gmail.com>
2024-12-03 10:23:18 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Navbar } from "./screens/components/navbar";
-import { SideBar } from "./screens/components/sidebar";
 import { HomeScreen } from "./screens/home/HomeScreen";
 
 export default function Home() {
